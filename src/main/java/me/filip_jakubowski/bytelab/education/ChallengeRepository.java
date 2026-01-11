a06cd88e8dd96c81f07cb3a70e04da2dd95f2b59
Filipo009 <jakfilipp@gmail.com>
2026-01-11 19:20:38 +0100

    Dodano zadanie realizacji prostej pętli for

--- src/main/java/me/filip_jakubowski/bytelab/education/ChallengeRepository.java
+++ src/main/java/me/filip_jakubowski/bytelab/education/ChallengeRepository.java
@@ -14,7 +14,7 @@ public class ChallengeRepository {
                 "Dodawanie i Rejestr D", // Title
                 "Twoim zadaniem jest wprowadzenie dwóch dowolnych liczb do rejestrów A i B, dodanie ich do siebie, a następnie zapisanie wyniku w rejestrze REGD.", // Description
                 "1. Użyj instrukcji IN dla REGA.\n2. Użyj instrukcji IN dla REGB.\n3. Wykonaj ADD wskazując REGD jako cel.", // STEPS (Wskazówka)
-                "IN 1 REGA\nIN 5 REGB\nADD --- REGD" // SOLUTION (Kod)
+                "IN 0x1 REGA\nIN 0x5 REGB\nADD ---- REGD" // SOLUTION (Kod)
         ));
 
         // Przykład drugiego zadania
@@ -22,7 +22,7 @@ public class ChallengeRepository {
                 "Czyszczenie rejestrów",
                 "Wprowadź wartość do REGA, a następnie wyczyść go, używając do tego Rejestru Zerowego (REG0).",
                 "1. Wprowadź dowolną wartość do REGA.\n2. Użyj instrukcji MOV.\n3. Skopiuj wartość z REG0 do REGA.",
-                "IN 10 REGA\nMOV REG0 REGA"
+                "IN 0x10 REGA\nMOV REG0 REGA"
         ));
 
         challenges.add(new Challenge(
@@ -33,12 +33,12 @@ public class ChallengeRepository {
                         "3. Wykonaj XOR (REGA i REGB) i zapisz do REGD (wynik będzie różny od zera).\n" +
                         "4. Przenieś (MOV) wartość z REGC do REGB.\n" +
                         "5. Ponownie wykonaj XOR (REGA i REGB) do REGD. Tym razem 3 XOR 3 da wynik 0.",
-                "IN 3 REGA\n" +
-                        "IN 5 REGB\n" +
-                        "IN 3 REGC\n" +
-                        "XOR --- REGD\n" +
+                "IN 0x3 REGA\n" +
+                        "IN 0x5 REGB\n" +
+                        "IN 0x3 REGC\n" +
+                        "XOR ---- REGD\n" +
                         "MOV REGC REGB\n" +
-                        "XOR --- REGD"
+                        "XOR ---- REGD"
         ));
 
         challenges.add(new Challenge(
@@ -48,18 +48,38 @@ public class ChallengeRepository {
                         "2. Wykonaj dodawanie do REGB i przemieszczaj wartości między rejestrami A, C i D.\n" +
                         "3. Użyj XOR z rejestrem REG0, aby wpłynąć na flagę Zero.\n" +
                         "4. Wykorzystaj JZ 0, aby zmusić Program Counter (PC) do powrotu na adres startowy 0x00.",
-                "IN 2 REGA\n" +
-                        "IN 4 REGC\n" +
-                        "ADD --- REGB\n" +
+                "IN 0x2 REGA\n" +
+                        "IN 0x4 REGC\n" +
+                        "ADD ---- REGB\n" +
                         "MOV REGA REGD\n" +
                         "MOV REGC REGA\n" +
                         "XOR REGB REG0\n" +
-                        "JZ 0\n" +
+                        "JZ 0x0\n" +
                         "MOV REGD REGA\n" +
-                        "ADD --- REGB\n" +
+                        "ADD ---- REGB\n" +
                         "MOV REGC REGA\n" +
-                        "XOR --- REG0\n" +
-                        "JZ 0"
+                        "XOR ---- REG0\n" +
+                        "JZ 0x0"
+        ));
+
+        challenges.add(new Challenge(
+                "Implementacja pętli sterowanej",
+                "Twoim celem jest stworzenie programu, który będzie dodawał wartości w pętli. Wykorzystamy instrukcję JUMP do powrotu oraz JZ do przerwania pętli i wypisania wyniku na wyjście (OUT), gdy osiągniemy określony stan.",
+                "1. Zainicjalizuj rejestry B i C wartościami początkowymi (licznik i krok).\n" +
+                        "2. Wykonaj operację ADD, która będzie kumulować wynik w REGA.\n" +
+                        "3. Użyj rejestru D jako tymczasowego schowka (MOV), aby móc manipulować danymi w REGB.\n" +
+                        "4. Wykonaj test logiczny XOR z REG0, aby sprawdzić stan procesora.\n" +
+                        "5. Użyj JZ 0x9, aby wyskoczyć z pętli do instrukcji OUT, lub JUMP 0x2, aby powtórzyć dodawanie.",
+                "IN 0x1 REGB\n" +
+                        "IN 0xA REGC\n" +
+                        "ADD ---- REGA\n" +
+                        "MOV REGB REGD\n" +
+                        "MOV REGC REGB\n" +
+                        "XOR ---- REG0\n" +
+                        "MOV REGD REGB\n" +
+                        "JZ 0x9\n" +
+                        "JUMP 0x2\n" +
+                        "OUT REGA"
         ));
     }
 
